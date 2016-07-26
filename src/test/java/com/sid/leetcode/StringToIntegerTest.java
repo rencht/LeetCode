bb3f0c8be41f645401a1da59a8ec1c3c4ccc4796
rencht <rencht4@gmail.com>
2016-07-26 12:42:43 +0800

    Add solution to #8 String to Integer (atoi)

---
+++ src/test/java/com/sid/leetcode/StringToIntegerTest.java
@@ -0,0 +1,42 @@
+package com.sid.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringToIntegerTest {
+
+	private StringToInteger problem;
+
+	@Before
+	public void setUp() throws Exception {
+		problem = new StringToInteger();
+	}
+
+	@Test
+	public void testMyAtoi() throws Exception {
+		assertEquals(Integer.MIN_VALUE, problem.myAtoi("-2147483648"));
+		assertEquals(-2147483647, problem.myAtoi("-2147483647"));
+		assertEquals(-1234, problem.myAtoi("-1234"));
+		assertEquals(-1, problem.myAtoi("-1"));
+		assertEquals(0, problem.myAtoi("0"));
+		assertEquals(1, problem.myAtoi("1"));
+		assertEquals(1234, problem.myAtoi("1234"));
+		assertEquals(Integer.MAX_VALUE, problem.myAtoi("2147483647"));
+
+		assertEquals(1, problem.myAtoi("+1"));
+		assertEquals(1234, problem.myAtoi("+1234"));
+		assertEquals(Integer.MAX_VALUE, problem.myAtoi("+2147483647"));
+
+		assertEquals(Integer.MIN_VALUE, problem.myAtoi("-2147483649"));
+		assertEquals(Integer.MAX_VALUE, problem.myAtoi("2147483648"));
+
+		assertEquals(10, problem.myAtoi("    010"));
+		assertEquals(-123, problem.myAtoi("-123  	"));
+
+		assertEquals(0, problem.myAtoi("+-2112"));
+		assertEquals(22, problem.myAtoi("22abc"));
+	}
+
+}
