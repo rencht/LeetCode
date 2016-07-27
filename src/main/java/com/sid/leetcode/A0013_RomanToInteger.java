2b81f5804edee8b157e7937351d1225eea131ba7
rencht <rencht4@gmail.com>
2016-07-27 17:10:45 +0800

    Add solution to #13 Roman to Integer

---
+++ src/main/java/com/sid/leetcode/A0013_RomanToInteger.java
@@ -0,0 +1,54 @@
+package com.sid.leetcode;
+
+/**
+ * 13. Roman to Integer.
+ *
+ * <blockquote>
+ * Given a roman numeral, convert it to an integer.
+ * <p>Input is guaranteed to be within the range from 1 to 3999.
+ * </blockquote>
+ *
+ * @author Sid.Chen
+ * @version 1.0, 2016-07-27
+ *
+ */
+public class A0013_RomanToInteger {
+
+	public int romanToInt(String s) {
+		final int[] INTEGER = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+		final String[] ROMAN = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+		int integer = 0;
+		for (int i = 0; i < ROMAN.length; i++) {
+			while (s.startsWith(ROMAN[i])) {
+				integer += INTEGER[i];
+				s = s.substring(ROMAN[i].length());
+			}
+		}
+		return integer;
+	}
+
+	public int optimizedRomanToInt(String s) {
+		final int[] ROMAN = new int[26];
+		ROMAN['M' - 'A'] = 1000;
+		ROMAN['D' - 'A'] = 500;
+		ROMAN['C' - 'A'] = 100;
+		ROMAN['L' - 'A'] = 50;
+		ROMAN['X' - 'A'] = 10;
+		ROMAN['V' - 'A'] = 5;
+		ROMAN['I' - 'A'] = 1;
+
+		int integer = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (i == s.length() - 1) {
+				integer += ROMAN[s.charAt(i) - 'A'];
+			} else if (ROMAN[s.charAt(i) - 'A'] < ROMAN[s.charAt(i + 1) - 'A']) {
+				integer -= ROMAN[s.charAt(i) - 'A'];
+			} else {
+				integer += ROMAN[s.charAt(i) - 'A'];
+			}
+		}
+		return integer;
+	}
+
+}
---
+++ src/test/java/com/sid/leetcode/A0013_RomanToIntegerTest.java
@@ -0,0 +1,57 @@
+package com.sid.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class A0013_RomanToIntegerTest {
+
+	private A0013_RomanToInteger problem;
+
+	@Before
+	public void setUp() throws Exception {
+		problem = new A0013_RomanToInteger();
+	}
+
+	@Test
+	public void testRomanToInt() throws Exception {
+		assertEquals(1, problem.romanToInt("I"));
+		assertEquals(2, problem.romanToInt("II"));
+		assertEquals(4, problem.romanToInt("IV"));
+		assertEquals(5, problem.romanToInt("V"));
+		assertEquals(8, problem.romanToInt("VIII"));
+		assertEquals(9, problem.romanToInt("IX"));
+		assertEquals(10, problem.romanToInt("X"));
+		assertEquals(14, problem.romanToInt("XIV"));
+		assertEquals(19, problem.romanToInt("XIX"));
+		assertEquals(200, problem.romanToInt("CC"));
+		assertEquals(800, problem.romanToInt("DCCC"));
+		assertEquals(1888, problem.romanToInt("MDCCCLXXXVIII"));
+		assertEquals(1899, problem.romanToInt("MDCCCXCIX"));
+		assertEquals(1976, problem.romanToInt("MCMLXXVI"));
+		assertEquals(1984, problem.romanToInt("MCMLXXXIV"));
+		assertEquals(3999, problem.romanToInt("MMMCMXCIX"));
+	}
+
+	@Test
+	public void testOptimizedRomanToInt() throws Exception {
+		assertEquals(1, problem.optimizedRomanToInt("I"));
+		assertEquals(2, problem.optimizedRomanToInt("II"));
+		assertEquals(4, problem.optimizedRomanToInt("IV"));
+		assertEquals(5, problem.optimizedRomanToInt("V"));
+		assertEquals(8, problem.optimizedRomanToInt("VIII"));
+		assertEquals(9, problem.optimizedRomanToInt("IX"));
+		assertEquals(10, problem.optimizedRomanToInt("X"));
+		assertEquals(14, problem.optimizedRomanToInt("XIV"));
+		assertEquals(19, problem.optimizedRomanToInt("XIX"));
+		assertEquals(200, problem.optimizedRomanToInt("CC"));
+		assertEquals(800, problem.optimizedRomanToInt("DCCC"));
+		assertEquals(1888, problem.optimizedRomanToInt("MDCCCLXXXVIII"));
+		assertEquals(1899, problem.optimizedRomanToInt("MDCCCXCIX"));
+		assertEquals(1976, problem.optimizedRomanToInt("MCMLXXVI"));
+		assertEquals(1984, problem.optimizedRomanToInt("MCMLXXXIV"));
+		assertEquals(3999, problem.optimizedRomanToInt("MMMCMXCIX"));
+	}
+
+}
