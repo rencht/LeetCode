d59673cad7bf5b5f930ab8713644abef77266a0d
rencht <rencht4@gmail.com>
2019-06-29 15:19:02 +0800

    Add solution to #6 ZigZag Conversion

--- src/main/java/com/sid/leetcode/A0006_ZigZagConversion.java
+++ src/main/java/com/sid/leetcode/A0006_ZigZagConversion.java
@@ -19,45 +19,66 @@ package com.sid.leetcode;
  * <p>Write the code that will take a string and make this conversion given a number of rows:
  *
  * <blockquote>
- * string convert(string text, int nRows);
+ * string convert(string s, int numRows);
  * </blockquote>
  *
- * <font color='#D02572'>convert("PAYPALISHIRING", 3)</font> should return <font color='#D02572'>"PAHNAPLSIIGYIR"</font>.
+ * <p>
+ * <b>Example 1:</b>
+ * <blockquote>
+ * <b>Input:</b> s = "PAYPALISHIRING", numRows = 3
+ * <p><b>Output:</b> "PAHNAPLSIIGYIR"
+ * </blockquote>
+ * 
+ * <p>
+ * <b>Example 2:</b>
+ * <blockquote>
+ * <b>Input:</b> s = "PAYPALISHIRING", numRows = 4
+ * <p><b>Output:</b> "PINALSIGYAHRPI"
+ * <p><b>Explanation:</b>
+ * <table>
+ * <tr align='center'><td>P</td><td></td><td></td><td>I</td><td></td><td></td><td>N</td></tr>
+ * <tr align='center'><td>A</td><td></td><td>L</td><td>S</td><td></td><td>I</td><td>G</td></tr>
+ * <tr align='center'><td>Y</td><td>A</td><td></td><td>H</td><td>R</td><td></td><td></td></tr>
+ * <tr align='center'><td>P</td><td></td><td></td><td>I</td><td></td><td></td><td></td></tr>
+ * </table>
+ * </blockquote>
  * </blockquote>
  *
  * @author Sid.Chen
- * @version 1.0, 2016-07-25
+ * @version 1.1, 2019-06-29
  *
  */
 public class A0006_ZigZagConversion {
 
-	public String convert(String s, int numRows) {
+	public String convert(final String s, final int numRows) {
 		if (s.length() <= numRows || numRows == 1) {
-			return s;
-		}
-		final int LENGTH = s.length();
-		final int OFFSET = (numRows - 1) << 1;
+            return s;
+        }
 
-		StringBuilder buffer = new StringBuilder();
-		int j = 0, k = 0;
-		for (int i = 0; i < numRows; i++) {
-			j = i;
-			if (i == 0 || i == numRows - 1) {
-				while (j < LENGTH) {
-					buffer.append(s.charAt(j));
-					j += OFFSET;
-				}
-			} else {
-				k = j - (i << 1);
-				while (j < LENGTH || k < LENGTH) {
-					buffer.append((k > 0 && k < LENGTH) ? s.charAt(k) : "");
-					buffer.append((j < LENGTH) ? s.charAt(j) : "");
-					k += OFFSET;
-					j += OFFSET;
-				}
-			}
-		}
-		return buffer.toString();
+        final StringBuilder buffer = new StringBuilder();
+
+        for (int i = 0; i < numRows; i++) {
+            int j = i;
+
+            int d1 = (numRows - i - 1) * 2;
+            int d2 = i * 2;
+
+            while (j < s.length()) {
+                buffer.append(s.charAt(j));
+                if (d1 == 0 || d2 == 0) {
+                    j += d1 + d2;
+                }
+                else {
+                    j += d1;
+
+                    if (j < s.length()) {
+                        buffer.append(s.charAt(j));
+                    }
+                    j += d2;
+                }
+            }
+        }
+        return buffer.toString();
 	}
 
 }
